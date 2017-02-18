8572912d17a38f30cd4e184b5b78d82a1f6a2bf7
Mandar <razzormandar@gmail.com>
2017-02-18 21:16:34 +0530

    Started using git on ubuntu

--- mnist_basic.py
+++ mnist_basic.py
@@ -13,7 +13,7 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
-
+#cross_entropy2 = tf.nn.softmax_cross_entropy_with_logits(tf.matmul(x, W) + b)
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess  = tf.InteractiveSession()
